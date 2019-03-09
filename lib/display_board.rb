24d56b153955208b30ecf51803147e99c119fec5
SOYEONG OH <axionoso@gmail.com>
2019-03-09 05:11:42 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,10 +1,8 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board()
-  puts "Welcome to Tic Tac Toe\n"
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
-  puts "What move would O like to make (1-9)?"
 end
